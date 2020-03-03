120c4368c33cb130cea892c7eb9b672f37322b97
fire1ce <stas@3os.org>
2020-03-03 12:02:58 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # macOSscreenControlServer
+
+## Install
+
+Clone the repository
+cd to the folder
+
+```bash
+npm install
+```
+
+Run the server:
+
+```bash
+node screenControlServer.js
+```
+
+Use:
+
+Server is listens on the 8888 port
+
+Turn On the MacOS Screen:
+"POST" requests
+http://<HOST>:8888/on
+
+Turn OFF the MacOS Screen:
+"POST" requests
+http://<HOST>:8888/off
+
+Get Status
+"POST" requests
+http://<HOST>:8888/status
+
+response:
+1 = on
+0 = off
---
+++ screenControlServer.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const { exec } = require('child_process');
+const port = 8888;
+
+app.post('/on', function(req, res) {
+    res.send('on');
+    exec('caffeinate -u -t 1');
+    console.log('on');
+});
+app.post('/off', function(req, res) {
+    res.send('off');
+    exec('pmset displaysleepnow');
+    console.log('off');
+});
+app.post('/status', function(req, res) {
+    exec(
+        'pmset -g powerstate IODisplayWrangler | tail -1 | cut -c29',
+        (error, stdout, stderr) => {
+            if (stdout == 4) {
+                res.send('1');
+                console.log('1');
+            } else {
+                res.send('0');
+                console.log('0');
+            }
+        }
+    );
+});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
