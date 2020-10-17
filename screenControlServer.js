cdf8897322c5157eefddf84d663a75da3fccf415
fire1ce <stas@3os.org>
2020-10-17 11:58:31 +0300

    update

--- screenControlServer.js
+++ screenControlServer.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const app = express();
-const { exec } = require('child_process');
-const port = 8888;
+const {exec} = require('child_process');
+const port = 55888;
 
-app.post('/on', function(req, res) {
+app.post('/on', function (req, res) {
     res.send('on');
     exec('caffeinate -u -t 1');
     console.log('on');
 });
-app.post('/off', function(req, res) {
+app.post('/off', function (req, res) {
     res.send('off');
     exec('pmset displaysleepnow');
     console.log('off');
 });
-app.post('/status', function(req, res) {
+app.post('/status', function (req, res) {
     exec(
         'pmset -g powerstate IODisplayWrangler | tail -1 | cut -c29',
         (error, stdout, stderr) => {
@@ -24,7 +24,7 @@ app.post('/status', function(req, res) {
                 res.send('0');
                 console.log('0');
             }
-        }
+        },
     );
 });
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`screenControl listening on port ${port}!`));
